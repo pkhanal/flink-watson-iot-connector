2cd0da54b02f3c2aa59a48c0b30b438867d4bf03
Prashant Khanal <prashantkhanal@gmail.com>
2017-06-21 15:52:28 -0700

    Update

--- src/main/java/io/github/pkhanal/MQTTSource.java
+++ src/main/java/io/github/pkhanal/MQTTSource.java
@@ -1,16 +1,18 @@
 package io.github.pkhanal;
 
 import org.apache.flink.api.common.functions.StoppableFunction;
+import org.apache.flink.hadoop.shaded.com.google.common.base.Charsets;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.eclipse.paho.client.mqttv3.*;
 
 import java.util.Properties;
 
-public class MQTTSource implements SourceFunction<String >, StoppableFunction, MqttCallback {
+public class MQTTSource implements SourceFunction<String >, StoppableFunction {
 
     // ----- Required property keys
     public static final String URL = "mqtt.broker.url";
     public static final String CLIENT_ID = "mqtt.client.id";
+    public static final String TOPIC = "mqtt.client.topic";
 
     // ------ Optional property keys
     public static final String USERNAME = "mqtt.username";
@@ -22,10 +24,12 @@ public class MQTTSource implements SourceFunction<String >, StoppableFunction, M
     // ------ Runtime fields
     private transient MqttClient client;
     private transient volatile boolean running;
+    private transient Object waitLock = new Object();
 
     public MQTTSource(Properties properties) {
         checkProperty(properties, URL);
         checkProperty(properties, CLIENT_ID);
+        checkProperty(properties, TOPIC);
 
         this.properties = properties;
     }
@@ -36,14 +40,13 @@ public class MQTTSource implements SourceFunction<String >, StoppableFunction, M
         }
     }
 
-
     @Override
     public void stop() {
-
+        close();
     }
 
     @Override
-    public void run(SourceContext<String> sourceContext) throws Exception {
+    public void run(SourceContext<String> ctx) throws Exception {
         MqttConnectOptions connectOptions = new MqttConnectOptions();
         connectOptions.setCleanSession(true);
 
@@ -58,25 +61,38 @@ public class MQTTSource implements SourceFunction<String >, StoppableFunction, M
         connectOptions.setAutomaticReconnect(true);
 
         client = new MqttClient(properties.getProperty(URL), properties.getProperty(CLIENT_ID));
+        client.connect(connectOptions);
+
+        client.subscribe(properties.getProperty(TOPIC), 0, (topic, message) -> {
+            synchronized (ctx.getCheckpointLock()) {
+                ctx.collect(new String(message.getPayload(), Charsets.UTF_8));
+            }
+        });
+
+        while (running) {
+            synchronized (waitLock) {
+                waitLock.wait(100L);
+            }
+        }
     }
 
     @Override
     public void cancel() {
-
+        close();
     }
 
-    @Override
-    public void connectionLost(Throwable throwable) {
+    private void close() {
+        this.running = false;
+        if (client != null) {
+            try {
+                client.disconnect();
+            } catch (MqttException exception) {
 
-    }
-
-    @Override
-    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-
-    }
-
-    @Override
-    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-
+            }
+        }
+        // leave main method
+        synchronized (waitLock) {
+            waitLock.notify();
+        }
     }
 }
